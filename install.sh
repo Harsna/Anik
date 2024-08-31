8f59a2948515b163d18613c9f068d3cbc8c5bb87
Harsna <Harshunako134@outlook.com>
2024-08-31 23:58:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/nova/g.py",
+    "postStartCommand": "python3 /workspaces/Anik/g.py",
     "customizations": {
         "vscode": {
             "settings": {
